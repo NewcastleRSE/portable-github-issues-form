53d04f850d06c5a6073d4bf30fe547db2ce1d1b2
Mark Turner <nmdt3@18DCompL027.local>
2020-07-03 15:51:54 +0100

    tweaked layout and added extra input fields

--- bundle.js
+++ bundle.js
@@ -18,7 +18,7 @@ window.GithubIssueForm = function (options) {
     function send(title, content) {
         // Basic validation:
         if (!title || !content) {
-            var feedback = 'Error: please answer all questions before submitting your issue.';
+            var feedback = 'Please answer all questions before submitting your feedback.';
             alert(feedback);
             throw new Error(feedback);
         }
@@ -32,7 +32,7 @@ window.GithubIssueForm = function (options) {
                 console.info(response.html_url, 'redirect url 2');
                 issue.html_url = response.html_url;
                 issue.DOMElement.reset();
-                var ask = window.confirm("Issue sent. Thank you! Would you like to view it ?");
+                var ask = window.confirm("Thank you for your feedback!");
                 if (ask) {
                     window.location.href = response.html_url;
                 }
@@ -64,24 +64,26 @@ window.GithubIssueForm = function (options) {
     this.form = ['<div id="js-github-issues-form" class="portable-github-form">',
         '<input id="open-item" name="js-githubform-trigger" type="radio" />',
         '<input id="close-item" name="js-githubform-trigger" type="radio" checked="checked" />',
-        '    <label for="open-item" class="open">Found a bug ?</label>',
+        '    <label for="open-item" class="open">Submit Feedback</label>',
         '<section>',
         '<label for="close-item" class="close">&times;</label>',
-        '<span class="title">Report a bug</span>',
+        '<span class="title">Submit Feedback</span>',
         '<div class="wrap">',
-        '<p class="info">Think you have found a bug? Please tell our developer all about it. Thank you for your time! (<a href="//github.com/' + this.repository + '/issues" target="_blank">See pending issues</a>)</p>',
+        '<p class="info">Found a bug or want to leave feedback?</p>',
+        '<p class="info">Please use this form to leave feedback about specific features or behaviours. (<a href="//github.com/' + this.repository + '/issues" target="_blank">See pending issues</a>)</p>',
         '<form id="js-github-form">',
-        '<fieldset>',
-        '<div><label for="git-title">Describe your issue in a few words</label><input type="text" id="git-title" name="title"></div>',
-        '<div><label for="git-content">Tell us everything about it</label><textarea id="git-content" name="content" placeholder="Please describe steps to reproduce the issue, include any error message..."></textarea>',
-        '</div><div><input tabindex="2" type="submit" name="send" id="js-github-issues-form-send" class="github-issues-form-send" value="Send this issue"></div>',
-        ' </fieldset></form>',
+        '<div><label for="git-name">Name</label><input type="text" id="git-name" name="name" placeholder="Your name"></div>',
+        '<div><label for="git-email">Email</label><input type="email" id="git-email" name="email" placeholder="Your email address"></div>',
+        '<div><label for="git-title">Title</label><input type="text" id="git-title" name="title" placeholder="Describe your feedback in a few words"></div>',
+        '<div><label for="git-content">Feedback</label><textarea id="git-content" name="content" placeholder="If reporting a bug, please describe steps to reproduce the issue, include any error messages."></textarea>',
+        '</div><div><input tabindex="2" type="submit" name="send" id="js-github-issues-form-send" class="github-issues-form-send" value="Submit"></div>',
+        '</form>',
         '</div></section></div>'].join('\n');
     this.addStyle = function () {
         // see scss file for a human-friendly version of the css code.
         var css = document.createElement("style");
         css.type = "text/css";
-        css.innerHTML = '.portable-github-form{position:fixed;bottom:-33px;right:0;z-index:9999;width:320px;height:auto;max-width:80%;padding:1.5rem;margin:2rem;font-family:sans-serif;font-size:1rem}.portable-github-form input[type=radio]{display:none}.portable-github-form label.close,.portable-github-form label.open{text-align:center;position:absolute}.portable-github-form label.open{font-size:11px;color:#222;background:#fff;width:100px;bottom:-50px;left:0;right:0;top:auto;margin:0 auto;padding:10px 0;text-transform:uppercase;z-index:1;font-weight:600;box-shadow:2px 4px 56px -6px rgba(0,0,0,.80)}.portable-github-form input#close-item:checked~label.open{-webkit-transition:bottom .3s ease .4s,background .1s ease,color .1s ease;-moz-transition:bottom .3s ease .4s,background .1s ease,color .1s ease;-ms-transition:bottom .3s ease .4s,background .1s ease,color .1s ease;-o-transition:bottom .3s ease .4s,background .1s ease,color .1s ease;transition:bottom .3s ease .4s,background .1s ease,color .1s ease;bottom:0}.portable-github-form label.open:focus,.portable-github-form label.open:hover{background:#222;color:#fff}.portable-github-form label.close{right:0;left:auto;top:0;bottom:auto;font-size:20px;background:#333;color:#fff;width:22px}.portable-github-form label.close:focus,.portable-github-form label.close:hover{background:#757786}.portable-github-form .wrap{padding:1rem}.portable-github-form section{margin:0;overflow:hidden;width:100%;height:auto;right:0;bottom:0;position:absolute;z-index:10;background:#fff;box-shadow:2px 4px 56px -6px rgba(0,0,0,.27);-webkit-backface-visibility:hidden;transition:all .4s cubic-bezier(.2,.6,.3,1)}.portable-github-form span.title{font-size:24px;padding:30px;color:#fff;background:#333;text-transform:uppercase;display:block;width:100%;font-weight:100}.portable-github-form p.info{font-size:13px;color:#999;line-height:18px}.portable-github-form input#close-item:checked~section,.portable-github-form section{transform:translateY(100%)}.portable-github-form input#open-item:checked~section{transform:translateY(0)}.portable-github-form fieldset{border:1px solid #DDD;padding:2rem}.portable-github-form legend{background:#fff;padding-right:1rem;display:block;margin-left:0}.portable-github-form div{margin:0 auto 1rem}.portable-github-form label{cursor:pointer;font-weight:700}.portable-github-form input,.portable-github-form label,.portable-github-form textarea{display:block;width:100%;font-size:.8rem;padding:5px}.portable-github-form textarea{min-height:6rem}.portable-github-form input.github-issues-form-send{font-size:1.5rem;width:auto;margin:auto}';
+        css.innerHTML = '.portable-github-form{position:fixed;bottom:-33px;right:0;z-index:9999;width:22rem;height:auto;max-width:80%;padding:1.5rem;margin:2rem;font-family:sans-serif;font-size:1rem}.portable-github-form input[type=radio]{display:none}.portable-github-form label.close,.portable-github-form label.open{text-align:center;position:absolute}.portable-github-form label.open{font-size:11px;color:#222;background:#fff;width:100px;bottom:-50px;left:0;right:0;top:auto;margin:0 auto;padding:10px 0;text-transform:uppercase;z-index:1;font-weight:600;box-shadow:2px 4px 56px -6px rgba(0,0,0,.80)}.portable-github-form input#close-item:checked~label.open{-webkit-transition:bottom .3s ease .4s,background .1s ease,color .1s ease;-moz-transition:bottom .3s ease .4s,background .1s ease,color .1s ease;-ms-transition:bottom .3s ease .4s,background .1s ease,color .1s ease;-o-transition:bottom .3s ease .4s,background .1s ease,color .1s ease;transition:bottom .3s ease .4s,background .1s ease,color .1s ease;bottom:0}.portable-github-form label.open:focus,.portable-github-form label.open:hover{background:#222;color:#fff}.portable-github-form label.close{right:0;left:auto;top:0;bottom:auto;font-size:20px;background:#333;color:#fff;width:22px}.portable-github-form label.close:focus,.portable-github-form label.close:hover{background:#757786}.portable-github-form .wrap{padding:1rem}.portable-github-form section{margin:0;overflow:hidden;width:100%;height:auto;right:0;bottom:0;position:absolute;z-index:10;background:#fff;box-shadow:2px 4px 56px -6px rgba(0,0,0,.27);-webkit-backface-visibility:hidden;transition:all .4s cubic-bezier(.2,.6,.3,1)}.portable-github-form span.title{font-size:24px;padding:30px;color:#fff;background:#333;text-transform:uppercase;display:block;width:100%;font-weight:100}.portable-github-form p.info{font-size:13px;color:#999;line-height:18px}.portable-github-form input#close-item:checked~section,.portable-github-form section{transform:translateY(100%)}.portable-github-form input#open-item:checked~section{transform:translateY(0)}.portable-github-form fieldset{border:1px solid #DDD;padding:2rem}.portable-github-form legend{background:#fff;padding-right:1rem;display:block;margin-left:0}.portable-github-form div{margin:0 auto 1rem}.portable-github-form label{display:inline-block;margin-bottom:0.5rem}.portable-github-form input,.portable-github-form textarea{display:block;width:21rem;font-family: Verdana,Geneva,sans-serif;padding: .375rem .75rem;font-size: 1rem;font-weight: 400;line-height: 1.5;color: #495057;background-color: #fff;background-clip: padding-box;border: 1px solid #ced4da;border-radius: .25rem;}.portable-github-form textarea{min-height:6rem}.portable-github-form input.github-issues-form-send{display: block;margin:2rem auto;font-weight: 400;color: #fff;text-align: center;vertical-align: middle;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;background-color: transparent;border: 1px solid transparent;padding: .375rem .75rem;font-size: 1rem;line-height: 1.5;border-radius: .25rem;background-color: #007bff;border-color: #007bff;}';
         document.head.appendChild(css);
     }
 
@@ -101,7 +103,9 @@ window.GithubIssueForm = function (options) {
             issue.DOMElement.addEventListener("submit", function (e) {
                 e.preventDefault();
                 var title = document.getElementById('git-title').value;
-                var body = document.getElementById('git-content').value;
+                var body = '<b>Name: </b>' + document.getElementById('git-name').value + '<br />';
+                body += '<b>Email: </b>' + document.getElementById('git-email').value + '<br /><br />';
+                body += document.getElementById('git-content').value;
                 send(title, body);
             });
         };
